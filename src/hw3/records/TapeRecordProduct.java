6ed2d6389e0e8811ddd02449427119828baee34d
hargu <hargu@10.7.77.118>
2024-02-13 12:28:59 -0600

    Added TapeRecordProduct constructor and copy constructor.

--- src/hw3/records/TapeRecordProduct.java
+++ src/hw3/records/TapeRecordProduct.java
@@ -1,5 +1,13 @@
 package hw3.records;
 
-public class TapeRecordProduct extends MediaProduct{
+public class TapeRecordProduct extends MediaProduct {
+
+public TapeRecordProduct(String title, double price, int year, Genre genre) {
+	super(title, price, year, genre);
+}
+
+public TapeRecordProduct(CDRecordProduct tape) {
+	this(tape.title, tape.price, tape.year, tape.genre);
+}
 
 }
