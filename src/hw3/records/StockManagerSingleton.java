65cc60357e0c51ec6907e5d23942e47798879a4a
hargu <hargu@10.7.77.118>
2024-02-13 12:30:12 -0600

    Defined the StockManagerSingleton
    Added private fields for instance and inventoryFilePath. Created a
    private constructor, and a getInstance() method.

--- src/hw3/records/StockManagerSingleton.java
+++ src/hw3/records/StockManagerSingleton.java
@@ -2,4 +2,20 @@ package hw3.records;
 
 public class StockManagerSingleton {
 
+	private static StockManagerSingleton instance;
+	private final String inventoryFilePath = "inventory.csv";
+	
+	private StockManagerSingleton() {
+	}
+	
+	public static StockManagerSingleton getInstance() {
+		
+		//create the instance if it doesn't exist yet
+        if (instance == null) {
+            instance = new StockManagerSingleton();
+        }
+        
+        return instance;
+	}
+	
 }
