11b008bc8aa735431bbeb40d0be7eb4acaa02f73
hargu <hargu@10.7.77.118>
2024-02-13 12:29:13 -0600

    Added CDRecordProduct constructor and copy constructor.

--- src/hw3/records/CDRecordProduct.java
+++ src/hw3/records/CDRecordProduct.java
@@ -2,4 +2,13 @@ package hw3.records;
 
 public class CDRecordProduct extends MediaProduct{
 
+	public CDRecordProduct(String title, double price, int year, Genre genre) {
+		super(title, price, year, genre);
+	}
+	
+	public CDRecordProduct(CDRecordProduct cd) {
+		this(cd.title, cd.price, cd.year, cd.genre);
+	}
+
+	
 }
