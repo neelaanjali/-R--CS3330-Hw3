e2d553faf1955641ffb85831a67ec54816fc6dad
hargu <hargu@10.7.77.118>
2024-02-13 12:27:50 -0600

    Defined MediaProduct class
    Added the protected fields for title, price, year, and genre. Created a
    constructor and the setters/getters.

--- src/hw3/records/MediaProduct.java
+++ src/hw3/records/MediaProduct.java
@@ -1,5 +1,54 @@
 package hw3.records;
 
 public abstract class MediaProduct {
+	
+	protected String title;
+	protected double price;
+	protected int year;
+	protected Genre genre;
+	
+	public MediaProduct(String title, double price, int year, Genre genre)
+	{
+		this.title = title;
+		this.price = price;
+		this.year = year;
+		this.genre = genre;
+	}
 
+	
+	
+	//setters and getters
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+	
+	
 }
