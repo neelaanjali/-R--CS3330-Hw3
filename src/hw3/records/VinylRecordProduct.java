0a6ccc249172b704cb1f55e6c4ce3e89508704ad
hargu <hargu@10.7.77.118>
2024-02-13 12:28:48 -0600

    Added VinylRecordProduct constructor and copy constructor.

--- src/hw3/records/VinylRecordProduct.java
+++ src/hw3/records/VinylRecordProduct.java
@@ -2,4 +2,12 @@ package hw3.records;
 
 public class VinylRecordProduct extends MediaProduct{
 
+	public VinylRecordProduct(String title, double price, int year, Genre genre) {
+		super(title, price, year, genre);
+	}
+	
+	public VinylRecordProduct(VinylRecordProduct vinyl) {
+		this(vinyl.title, vinyl.price, vinyl.year, vinyl.genre);
+	}
+
 }
