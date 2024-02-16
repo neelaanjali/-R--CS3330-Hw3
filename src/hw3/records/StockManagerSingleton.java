a5c86fc959dd5c2c7958db3f4955fbc4fb5ef50d
Neela Hanumanthiah <neelahanumanthiah@mu-584783.dhcp.missouri.edu>
2024-02-16 15:18:23 -0600

    Added InitializeStock, updateIemPrice, add item and remove item

--- src/hw3/records/StockManagerSingleton.java
+++ src/hw3/records/StockManagerSingleton.java
@@ -18,4 +18,46 @@ public class StockManagerSingleton {
         return instance;
 	}
 	
+	public boolean initializeStock() {
+	    try (BufferedReader br = new BufferedReader(new FileReader(inventoryFilePath))) {
+	        String line;
+	        while ((line = br.readLine()) != null) {
+	            String[] data = line.split(",");
+	            if (data.length == 4) {
+	                String title = data[0].trim();
+	                double price = Double.parseDouble(data[1].trim());
+	                int year = Integer.parseInt(data[2].trim());
+	                Genre genre = Genre.valueOf(data[3].trim());
+	                MediaProduct product = new MediaProduct(title, price, year, genre);
+	                inventory.add(product);
+	            }
+	        }
+	        return true;
+	    } catch (IOException | NumberFormatException | IllegalArgumentException e) {
+	        return false;
+	    }
+	}
+	
+
+	public boolean updateItemPrice(MediaProduct product, double newPrice) {
+	    for (MediaProduct object : inventory) {
+	        if (object.equals(product)) {
+	            object.price = newPrice;
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+
+	
+	public boolean addItem(MediaProduct product) {
+	    return inventory.add(new MediaProduct(product));
+	} 
+
+	
+
+	public boolean removeItem(MediaProduct product) {
+	    return inventory.remove(product);
+	}
+
 }
