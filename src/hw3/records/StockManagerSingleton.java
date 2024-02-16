49ea9a246a9461efc7aa751314da868ef7acd674
jxie26 <jessxie54@gmail.com>
2024-02-16 15:42:19 -0600

    set up 3 methods

--- src/hw3/records/StockManagerSingleton.java
+++ src/hw3/records/StockManagerSingleton.java
@@ -18,4 +18,28 @@ public class StockManagerSingleton {
         return instance;
 	}
 	
+	
+	//Saves the updated inventory back to the CSV file located at inventoryFilePath.
+			//Overwrites the existing file with the updated inventory data.
+			//Returns true if the saving is successful, false otherwise (file does not exist, or file empty)
+	public boolean saveStock() {
+		for (MediaProduct product : inventory) {
+			
+		}
+		return false;
+	}
+	
+	public ArrayList<MediaProduct> getMediaProductBelowPrice(int maxPrice) {
+		//Gets the media products that are below the given maxPrice.
+		//This creates a new ArrayList of media products that is below the maxPrice. Beware of
+		//not leaking any informaHon
+		//return ;
+	}
+	
+	//Prints the given media product list
+	public void printListOfMediaProduct(ArrayList<MediaProduct> productList) {
+		System.out.println();
+	}
+	
+	
 }
