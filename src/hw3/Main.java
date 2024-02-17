7927a10113d0b4d835fce1501aa8113c4853e52e
hargu <hargu@Joes_Laptop>
2024-02-16 21:43:27 -0600

    Wrote Main method
    Added to Main to demonstrate some functionality of the program

--- src/hw3/Main.java
+++ src/hw3/Main.java
@@ -1,10 +1,37 @@
 package hw3;
 
+import java.util.ArrayList;
+
+import hw3.records.Genre;
+import hw3.records.StockManagerSingleton;
+import hw3.records.TapeRecordProduct;
+import hw3.records.MediaProduct;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		StockManagerSingleton manager = StockManagerSingleton.getInstance();
+		
+		manager.initializeStock();
+		
+		manager.printListOfMediaProduct(manager.getInventory());
+		
+		TapeRecordProduct tape = new TapeRecordProduct("Cherry", 14.99, 2015, Genre.POP);
+		
+		manager.addItem(tape);
+		
+		manager.printListOfMediaProduct(manager.getInventory());
+		
+		System.out.println();
+		manager.printListOfMediaProduct(manager.getMediaProductBelowPrice(10));
+		manager.updateItemPrice(tape, 9.99);
+		System.out.println();
+		manager.printListOfMediaProduct(manager.getMediaProductBelowPrice(10));
+		
+		manager.saveStock();
+		
+	}	
 
 }
